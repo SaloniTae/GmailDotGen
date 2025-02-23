33af1a98985bd0b43a03f833c2a539a1c0c39c9e
SaloniTae <98252350+SaloniTae@users.noreply.github.com>
2025-02-23 12:42:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,9 @@ app.get('/generate-email', async (req, res) => {
     }
 });
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
